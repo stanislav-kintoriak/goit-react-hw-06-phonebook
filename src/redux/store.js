2075e0f823f792ebd58fcc7ff74b161228917fcd
Stanislav Kintoriak <kintoriak@gmail.com>
2023-06-10 09:42:52 +0300

    Commit 8
    1) Used configureStore() in the store.js;

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,6 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
 
-import { rootReducer } from './reducer';
-
-const enhancer = devToolsEnhancer();
-
-export const store = createStore(rootReducer, enhancer);
+import { configureStore } from "@reduxjs/toolkit";
+import { rootReducer } from "./reducer";
+export const store = configureStore({
+  reducer: rootReducer,
+});
